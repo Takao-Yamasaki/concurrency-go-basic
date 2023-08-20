729d37e14875588d642488ba2b4c990ef1d8edee
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-08-20 10:47:59 +0900

    add:non-buffer

--- main.go
+++ main.go
@@ -2,37 +2,24 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	// "sync"
-	// "time"
+	"sync"
 )
 
 func main() {
-	// 	ch := make(chan int)
-	// 	var wg sync.WaitGroup
-	// 	wg.Add(1)
-	// 	go func() {
-	// 		defer wg.Done()
-	// 		ch <- 10
-	// 		time.Sleep(500 * time.Millisecond)
-	// 	}()
-	// 	fmt.Println(<-ch)
-	// 	wg.Wait()
-
 	ch1 := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
-		// 読み込み操作を行なっているが、書き込みがないので、ずっと待機している
+		defer wg.Done()
+		// チャネルの読み込み
 		fmt.Println(<-ch1)
 	}()
-	// 書き込みを追加してみる
+	// チャネルへの書き込み
 	ch1 <- 10
-	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
-
-	// バッファ付きchannel
-	ch2 := make(chan int, 1)
-	// 書き込み
-	ch2 <- 2
-	ch2 <- 3
-	// 読み込み
-	fmt.Println(<-ch2)
+	// チャネルを閉じる
+	close(ch1)
+	// チャネルが開いているか閉じているか確認
+	v, ok := <-ch1
+	fmt.Printf("%v %v\n", v, ok)
+	wg.Wait()
 }
